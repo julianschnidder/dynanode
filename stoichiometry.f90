68dfedab9a77bf08dc518835ab9b2338489d0f43
Julian Schnidder <julian.schnidder@gmail.com>
2015-01-06 22:40:26 +0100

    cleanups

--- stoichiometry.f90
+++ stoichiometry.f90
@@ -1,16 +1,4 @@
 module stoichiometry
     use double
     implicit none
-
-
-!    subroutine getSMatrix(nunit, S)
-!        integer, intent(in)::nunit
-!        real(kind=dp),allocatable,intent(out),dimension(:,:) :: S
-!
-!        integer:: numbsRe, numbsMet
-!        read (nunit) numbRxe, numbsMet
-!        allocate(S(numbsRe,numbsMet))
-!        read(nunit) S
-!    end subroutine getSMatrix
-
 end module stoichiometry
